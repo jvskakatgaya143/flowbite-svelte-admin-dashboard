28e7d916cf73a559fb8a8acc63949d8c4c83b643
jvskakatgaya143 <jvskakatgaya143@gmail.com>
2025-08-21 18:15:40 +0530

    feat: add liquid glass effect

--- src/routes/(no-sidebar)/+layout.svelte
+++ src/routes/(no-sidebar)/+layout.svelte
@@ -2,6 +2,7 @@
   import Footer from '../(no-sidebar)/Footer.svelte';
   import Navbar from '../(sidebar)/Navbar.svelte';
   import type { Snippet } from 'svelte';
+  import { LiquidGlass } from 'liquid-glass-svelte';
   import '../../app.css';
   interface Props {
     children: Snippet;
@@ -9,10 +10,12 @@
   let { children }: Props = $props();
 </script>
 
-<header class="fixed top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800">
-  <Navbar />
-</header>
-<div class="mx-auto max-w-screen-2xl pt-[70px]">
-  {@render children()}
-  <Footer />
-</div>
+<LiquidGlass class="min-h-screen">
+  <header class="fixed top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800">
+    <Navbar />
+  </header>
+  <div class="mx-auto max-w-screen-2xl pt-[70px]">
+    {@render children()}
+    <Footer />
+  </div>
+</LiquidGlass>
